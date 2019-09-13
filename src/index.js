a2258c8019bbce32974efe11d5595b8122f3e001
Ryan R. Smith <rsmith@fullcompass.com>
2019-09-13 18:24:11 -0500

    Getting rolling with WebPack.
    Signed-off-by: Ryan R. Smith <rsmith@fullcompass.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash';
+import './assets/css/style.css';
+import Icon from './assets/images/fpo_avatar.png';
+import Data from './data/data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
